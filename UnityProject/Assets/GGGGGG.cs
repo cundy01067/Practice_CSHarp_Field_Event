4632b35a8bd6a0525184247170e5529fd2012f96
cundy01067 <57181197+cundy01067@users.noreply.github.com>
2019-11-22 18:05:45 +0800

    完成定義欄位

---
+++ UnityProject/Assets/GGGGGG.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GGGGGG : MonoBehaviour
+{
+    [Header("移動速度") , Range(0 , 999)]
+    public float Speed = 10.0F;
+    [Header("跳躍高度") , Range(0 , 999)]
+    public int Jump = 200;
+    [Header("對話內容")]
+    public string Talk = "咕咕咕~~";
+    [Header("是否取得雞蛋")]
+    public bool Egg;
+
+
+
+    private void Start()
+    {
+        print("遊戲開始");
+    }
+
+
+    private void Update()
+    {
+        print("咕咕咕~~");
+    }
+}
